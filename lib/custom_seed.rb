77b52d3866c4f2279e1a0bb2e9a763ba920c9446
psahni <prashant.sahni5@gmail.com>
2013-04-29 18:49:13 +0530

    loading of files issue

--- lib/custom_seed.rb
+++ lib/custom_seed.rb
@@ -1,8 +1,8 @@
 
 require 'rails'
 
-require 'lib/custom_seed/seed'
-require 'lib/custom_seed/custom_seed_migration'
+require 'custom_seed/seed'
+require 'custom_seed/custom_seed_migration'
 require 'custom_seed/version'
 
 module CustomSeed
